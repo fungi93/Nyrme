5316be8f11e86d906eb023d6657c71369f4fa11d
fungi93 <91768344+fungi93@users.noreply.github.com>
2023-04-18 16:47:24 -0400

    Update restaurants.js

--- restaurants.js
+++ restaurants.js
@@ -1,64 +1,19 @@
-function getUserLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showRestaurants, errorCallback);
-  } else {
-    alert("Geolocation is not supported by this browser.");
-  }
-}
+import {fetch} from 'wix-fetch';
 
-function errorCallback(error) {
-  alert("Error occurred. Error code: " + error.code);
-}
-
-async function showRestaurants(position) {
-  const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  const userLocation = new google.maps.LatLng(lat, lng);
-
-  const map = new google.maps.Map(document.createElement('div')); // create a hidden map
-  const service = new google.maps.places.PlacesService(map);
-
-  const request = {
-    location: userLocation,
-    radius: 5000, // 5 km in meters
-    type: ['restaurant'],
-  };
-
-  service.nearbySearch(request, (results, status, pagination) => {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      const delay = 500; // 500 ms delay between requests
-      results.forEach((restaurant, index) => {
-        setTimeout(() => {
-          fetchRestaurantDetails(restaurant.place_id);
-        }, index * delay);
+$w.onReady(() => {
+  $w('#myButton').onClick(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+        const response = await fetch(`https://yourbackend.example.com/find_restaurants?lat=${lat}&lng=${lng}`);
+        const restaurants = await response.json();
+        // Display the restaurants
+      }, (error) => {
+        console.error('Error occurred. Error code: ' + error.code);
       });
-      if (pagination.hasNextPage) {
-        pagination.nextPage();
-      }
     } else {
-      alert("Error occurred: " + status);
+      console.error('Geolocation is not supported by this browser.');
     }
   });
-}
-
-function fetchRestaurantDetails(placeId) {
-  const map = new google.maps.Map(document.createElement('div'));
-  const service = new google.maps.places.PlacesService(map);
-
-  service.getDetails({ placeId }, (place, status) => {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      displayRestaurant(place);
-    } else {
-      alert("Error occurred: " + status);
-    }
-  });
-}
-
-function displayRestaurant(restaurant) {
-  const listContainer = document.getElementById('restaurants-list');
-  const listItem = document.createElement('li');
-  listItem.innerHTML = `<a href="${restaurant.website || '#'}" target="_blank">${restaurant.name}</a>
-                        <p>Rating: ${restaurant.rating}</p>
-                        <p>Website: <a href="${restaurant.website || '#'}" target="_blank">${restaurant.website || 'N/A'}</a></p>`;
-  listContainer.appendChild(listItem);
-}
+});
